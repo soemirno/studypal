98c69578bea89d0414393bc232b2435dd0754af0
soemirno <soemirno@gmail.com>
2007-08-18 15:37:12 +0000

    filled user object in user spec
    git-svn-id: https://studypal.googlecode.com/svn/trunk@57 a2258645-2a37-0410-af75-377fab155b91

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,20 +2,20 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Student do
   before(:each) do
-    @student = Student.new
+    @student = Student.new :login => "Albert", :identity_url => "http://myopenid/url" , :email => "albert@someplace.com"
   end
 
   it "should have a login name" do
-    @student.login.should be_nil
+    @student.login.should == "Albert"
   end  
   
   it "should have an identity_url" do
-    @student.identity_url.should be_nil
+    @student.identity_url.should == "http://myopenid/url"
   end  
   
 
   it "should have an email address" do
-    @student.email.should be_nil
+    @student.email.should == "albert@someplace.com"
   end
   
 end
