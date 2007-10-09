9e0746aa9aa363c0bc248957b776f218f4aaf7b7
soemirno <soemirno@gmail.com>
2007-10-09 21:04:27 +0000

    added forgotten view specs
    git-svn-id: https://studypal.googlecode.com/svn/trunk@81 a2258645-2a37-0410-af75-377fab155b91

---
+++ spec/views/courses/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "/courses/new.html.erb" do
+  include CoursesHelper
+
+  before do
+    @course = mock_model(Course, :title => 'een titel', :description => 'een beschrijving')
+    @course.stub!(:new_record?).and_return(true)    
+    assigns[:course] = @course
+  end
+
+  it "should render new form" do
+    render "/courses/new.html.erb"
+    response.should have_tag("form[action=?][method='post']", "/courses") do
+      with_tag("input#course_title[name=?]", "course[title]")
+      with_tag("textarea#course_description[name=?]", "course[description]")
+      with_tag("input[type=submit]")
+    end
+  end  
+  
+end
